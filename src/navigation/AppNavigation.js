e87d79abb193f3e185b040a117f006f6a4c9ab01
Ilya Trenin <itrenin@gmail.com>
2022-02-20 18:03:48 +0300

    init commit

---
+++ src/navigation/AppNavigation.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { createAppContainer, ThemeColors } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { Platform } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import { MainScreen } from '../screens/MainScreen'
+import { PostScreen } from '../screens/PostScreen'
+import { BookedScreen } from '../screens/BookedScreen'
+import { THEME } from '../theme'
+
+const PostNavigator = createStackNavigator(
+  {
+    Main: MainScreen,
+    Post: {
+      screen: PostScreen,
+    },
+  },
+  {
+    initialRouteName: 'Main',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
+      },
+      headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
+    },
+  }
+)
+
+const BookedNavigator = createStackNavigator(
+  {
+    Booked: BookedScreen,
+    Post: PostScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
+      },
+      headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
+    },
+  }
+)
+
+const BottomNavigator = createBottomTabNavigator(
+  {
+    Post: {
+      screen: PostNavigator,
+      navigationOptions: {
+        tabBarIcon: (info) => (
+          <Ionicons name="ios-albums" size={25} color={info.tintColor} />
+        ),
+      },
+    },
+    Booked: {
+      screen: BookedNavigator,
+      navigationOptions: {
+        tabBarIcon: (info) => (
+          <Ionicons name="ios-star" size={25} color={info.tintColor} />
+        ),
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: THEME.MAIN_COLOR,
+    },
+  }
+)
+
+export const AppNavigation = createAppContainer(BottomNavigator)
---
+++ src/screens/MainScreen.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { View, Text, StyleSheet, Button, FlatList } from 'react-native'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import { DATA } from '../data'
+import { Post } from '../components/Post'
+import { AppHeaderIcon } from '../components/AppHeaderIcon'
+
+export const MainScreen = ({ navigation }) => {
+  const openPostHandler = post => {
+    navigation.navigate('Post', {
+      postId: post.id,
+      date: post.date,
+      booked: post.booked
+    })
+  }
+
+  return (
+    <View style={styles.wrapper}>
+      <FlatList
+        data={DATA}
+        keyExtractor={post => post.id.toString()}
+        renderItem={({ item }) => <Post post={item} onOpen={openPostHandler} />}
+      />
+    </View>
+  )
+}
+
+MainScreen.navigationOptions = {
+  headerTitle: 'Мой блог',
+  headerRight: (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title='Take photo'
+        iconName='ios-camera'
+        onPress={() => console.log('Press photo')}
+      />
+    </HeaderButtons>
+  ),
+  headerLeft: (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title='Toggle Drawer'
+        iconName='ios-menu'
+        onPress={() => console.log('Press photo')}
+      />
+    </HeaderButtons>
+  )
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    padding: 10
+  }
+})
